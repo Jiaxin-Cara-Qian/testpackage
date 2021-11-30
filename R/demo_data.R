939f60570204a25345f1ba21647ef5be40f7604e
Jiaxin-Cara-Qian <jiaxinq@umich.edu>
2021-11-30 20:10:28 +0800

    add sample_data

---
+++ R/demo_data.R
@@ -0,0 +1,18 @@
+#'@title Sample Data for Demostration
+#'
+#'@description A common measurement of lung function is the forced expiratory
+#'volume FEV, which measures how much air a person can blow out of
+#'the lungs in a short period of time. A higher FEV is usually
+#'associated with better respiratory function. A group of clinicians is
+#'interested in examining the factors which affect FEV1. Data on 654
+#'children are available for the following covariates: AGE (A, years),HEIGHT (H, inches),
+#'MALE (M=0 for female, 1 for male) SMOKE(Si=0 for non-smoker and 1 for smoker).
+#'Each row corresponds to a single clinic visit.
+#'
+#'
+#'@docType data
+#'
+#'@usage data(demo_data)
+#'
+#'@references Module K, BIOSTAT650, Dr. Xu Shi ,University of Michigan, School of Public Health
+"demo_data"
