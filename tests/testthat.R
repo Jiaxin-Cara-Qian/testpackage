f9440f76da3da0f73a89f821d7ceb9c839afefba
Jiaxin-Cara-Qian <jiaxinq@umich.edu>
2021-12-05 10:59:40 +0800

    deleted unused files
    square.R is for test only, and testhat is also deleted

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(testpackage)
-
-test_check("testpackage")
--- tests/testthat/test-square.R
+++
@@ -1,4 +0,0 @@
-test_that("square works", {
-  expect_equal(square(3), 9)
-  expect_equal(square(1:3), c(1,4,9))
-})
